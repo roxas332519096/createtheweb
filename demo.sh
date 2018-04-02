b4798563bca5afcc2e11e61821e988013458a3ca
roxas3325199096 <roxas3325199096@vip.qq.com>
2018-04-02 12:11:45 +0800

    2nd update

--- demo.sh
+++ demo.sh
@@ -6,12 +6,12 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo 'success'
   echo '<!DOCTYPE>'> index.html
   echo ' <title>Hello</title>' >> index.html
   echo '<h1>Hi</h1>' >> index.html
   echo 'h1{color:red;}' > css/style.css
   echo 'var string = "Hello World"' > js/main.js
   echo 'alert(string)' >> js/main.js
+  echo 'success'
   exit
 fi
